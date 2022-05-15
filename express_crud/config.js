0a00d7001a592fdbfe8a029249390cf06bbcf771
faiz <faizuddinansari53@gmail.com>
2022-05-15 21:39:33 +0530

    'changpassword'

--- express_crud/config.js
+++ express_crud/config.js
@@ -2,7 +2,7 @@
 
 let config = {
     dbUrl:
-    "mongodb+srv://faizuddin786:Ansari12345@cluster0.wt3w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://faizuddin786:******@cluster0.wt3w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   };
   
   module.exports = config;
